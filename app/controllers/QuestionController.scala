15e2bf21dbfe1a65553dab1de42be620f8824179
Ulises Fasoli <ufasoli@gmail.com>
2013-12-19 16:15:44 +0100

    added crud operations to controller and routes

---
+++ app/controllers/QuestionController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ulises Fasoli
+ * Date: 16.12.13
+ * Time: 13:29
+ *
+ */
+object QuestionController {
+
+}
---
+++ app/util/SimpleResponse.scala
@@ -0,0 +1,12 @@
+package util
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ulises Fasoli
+ * Date: 19.12.13
+ * Time: 11:47
+ *
+ */
+class SimpleResponse {
+
+}
